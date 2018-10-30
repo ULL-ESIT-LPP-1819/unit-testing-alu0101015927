285d0610bc23aef951dec236e367f0c916ce4dae
Marta García <alu0101015927@ull.edu.es>
2018-10-30 20:47:03 +0100

    primeras pruebas

--- lib/Nutricion.rb
+++ lib/Nutricion.rb
@@ -3,16 +3,16 @@ class Nutricion
 	attr_reader :peso, :talla, :edad, :sexo, :ccintura, :ccadera
 
 	def initialize(peso, talla, edad, sexo, ccintura, ccadera)
-		@peso = peso
-		@talla = talla
+		@peso = Float(peso)
+		@talla = Float(talla)
 		@edad = edad
 		@sexo = sexo
-		@ccintura = ccintura
-		@ccadera = ccadera
+		@ccintura = Float(ccintura)
+		@ccadera = Float(ccadera)
 	end
 
 	def indice_masa_corporal
-		peso / (talla * talla)
+		(peso / (talla * talla) * 10000).round(1)
 	end
 
 
@@ -21,50 +21,50 @@ class Nutricion
 
 
 		if x < 18.5
-			return "#{@x} < 18.5 -- Bajo peso -- Delgado"
+			return "#{x} < 18.5 -- Bajo peso -- Delgado"
 		elsif x >= 18.5 && x <= 24.9
-			return "18.5 < #{@x} < 24.9 -- Adecuado -- Aceptable"
+			return "18.5 < #{x} < 24.9 -- Adecuado -- Aceptable"
 		elsif x >= 25.0 && x <= 29.9
-			return "25.0 < #{@x} < 29.9 -- Sobrepeso -- Sobrepeso"
+			return "25.0 < #{x} < 29.9 -- Sobrepeso -- Sobrepeso"
 		elsif x >= 30.0 && x <= 34.9
-			return "30.0 < #{@x} < 34.9 -- Obesidad grado 1 -- Obesidad"
+			return "30.0 < #{x} < 34.9 -- Obesidad grado 1 -- Obesidad"
 		elsif x >= 40
-			return " #{@x} > 40 -- Obesidad grado 2 -- Obesidad"
+			return " #{x} > 40 -- Obesidad grado 2 -- Obesidad"
 		end
 
 	end
 
 	def porc_grasa
-		1.2 * self.indice_masa_corporal + 0.23 * edad - 10.8 * sexo - 5.4
+		(1.2 * self.indice_masa_corporal + 0.23 * edad - 10.8 * sexo - 5.4).round(1)
 	end
 
 	def relacion_cc
-		ccintura / ccadera
+		(ccintura / ccadera).round(1)
 	end
 
 
 	def rcc_tabla
 		x = self.relacion_cc
 
-			if x == 1
+			if sexo == 1
 					if x >= 0.83 && x < 0.88
-						return "Bajo -- 0.83 < #{@x} < 0.88"
+						return "Bajo -- 0.83 < #{x} < 0.88"
 					elsif x >= 0.88 && x < 0.95
-						return "Moderado -- 0.88 < #{@x} < 0.95"
+						return "Moderado -- 0.88 < #{x} < 0.95"
 					elsif x >= 0.95 && x < 1.01
-						return "Alto -- 0.95 < #{@x} < 1.01"
-					elsif x > 1.01
-						return "Muy alto -- #{@x} > 1.01"
+						return "Alto -- 0.95 < #{x} < 1.01"
+					elsif x >= 1.01
+						return "Muy alto -- #{x} > 1.01"
 					end
 			end
 
-			if x == 0
+			if sexo == 0
 						if x >= 0.72 && x < 0.75
-							return "Bajo -- 0.72 < #{@x} < 0.75"
-						elsif x >= 0.78 && x < 0.82
-							return "Moderado -- 0.78 < #{@x} < 0.82"
-						elsif x > 0.82
-							return "Alto -- #{@x} > 0.82"
+							return "Bajo -- 0.72 < #{x} < 0.75"
+						elsif x >= 0.75 && x < 0.82
+							return "Moderado -- 0.75 < #{x} < 0.82"
+						elsif x >= 0.82
+							return "Alto -- #{x} > 0.82"
 						end
 			end
 	end
--- test/tc_nutricion.rb
+++ test/tc_nutricion.rb
@@ -1,7 +1,30 @@
-require 'lib/Nutricion'
+require_relative '../lib/Nutricion'
 require 'test/unit'
 
 class TestNutricion < Test::Unit::TestCase
 
+  def setup
+    @persona1 = Nutricion.new(60, 167, 20, 0, 50, 53)
+  end
 
+  def test_persona
+
+    assert_equal(60, @persona1.peso)
+    assert_equal(167, @persona1.talla)
+    assert_equal(20, @persona1.edad)
+    assert_equal(0, @persona1.sexo)
+    assert_equal(50, @persona1.ccintura)
+    assert_equal(53, @persona1.ccadera)
+
+  end
+
+  def test_formulas
+
+    assert_equal(21.5, @persona1.indice_masa_corporal)
+    assert_equal("18.5 < 21.5 < 24.9 -- Adecuado -- Aceptable", @persona1.imc_tabla)
+    assert_equal(25, @persona1.porc_grasa)
+    assert_equal(0.9, @persona1.relacion_cc)
+    assert_equal("Alto -- 0.9 > 0.82", @persona1.rcc_tabla)
+
+  end
 end
